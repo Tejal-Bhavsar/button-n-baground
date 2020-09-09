b6c386a3414d0521f2ba04eb4d65ad5f9ab0bcf2
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2020-09-09 16:46:48 +0530

    Updates

---
+++ precache-manifest.8fea3aab23d396384fee5924f3c72532.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d186095a5f4bf5942018fc77e4253b26",
+    "url": "/button-bag/index.html"
+  },
+  {
+    "revision": "6dce961229196862bf49",
+    "url": "/button-bag/static/css/main.f66fa6dd.chunk.css"
+  },
+  {
+    "revision": "11ae1afc71753ed45f0e",
+    "url": "/button-bag/static/js/2.b10f7047.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/button-bag/static/js/2.b10f7047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dce961229196862bf49",
+    "url": "/button-bag/static/js/main.f4fce428.chunk.js"
+  },
+  {
+    "revision": "11620d761ea1eb866f59",
+    "url": "/button-bag/static/js/runtime-main.7ccd21cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/button-bag/static/media/logo.5d5d9eef.svg"
+  }
+]);
